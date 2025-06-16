4a102559ba4da2ae1e06247968f09bfe67a07b5b
Toffee Studio <contact@toffeestudio.com>
2025-06-16 15:09:21 +1000

    [MILESTONE]: barebones firmware created

---
+++ keyboards/toffee_studio/module_bare/module.c
@@ -0,0 +1,27 @@
+#include "quantum.h"
+#include "print.h"   // For uprintf
+
+led_config_t g_led_config = {
+    // The .matrix_co map can be left empty for this test.
+    {
+        {0}
+    },
+
+    // Define a dummy physical position for each of your 13 LEDs.
+    {
+        {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},
+        {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}
+    },
+
+    // Define a default flag for each of your 13 LEDs.
+    {
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT, LED_FLAG_KEYLIGHT,
+        LED_FLAG_KEYLIGHT
+    }
+};
+
+void keyboard_post_init_kb(void) {
+    uprintf("keyboard_post_init_kb called.\n");
+}
---
+++ keyboards/toffee_studio/module_bare/rules.mk
@@ -0,0 +1,3 @@
+RGB_MATRIX_ENABLE = yes
+RGBLIGHT_ENABLE = no           # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no
